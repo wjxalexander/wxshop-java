d96e52b81dc2733c1b84d48618a0700fa68a2dc7
jingxinwang <wjxalexander@hotmail.com>
2023-12-07 08:52:36 +0800

    checkstyle

--- src/main/java/com/jingxin/wxshop/WxshopApplication.java
+++ src/main/java/com/jingxin/wxshop/WxshopApplication.java
@@ -5,8 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WxshopApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(WxshopApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(WxshopApplication.class, args);
+    }
 
 }
--- src/test/java/com/jingxin/wxshop/WxshopApplicationTests.java
+++ src/test/java/com/jingxin/wxshop/WxshopApplicationTests.java
@@ -6,10 +6,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class WxshopApplicationTests {
+    @Test
+    void contextLoads() {
 
-	@Test
-	void contextLoads() {
-
-	}
+    }
 
 }
